924939b44bea35b2e3a4c3b83d48eab4073d2f34
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2017-07-11 16:17:59 -0600

    Add utils.py

---
+++ opendssdirect/utils.py
@@ -0,0 +1,18 @@
+import inspect
+
+
+class Iterator(object):
+    def __init__(self, module, function):
+        assert inspect.ismodule(module), '{module} must be of type module'.format(module=module)
+        self.module = module
+        self.function = function
+
+    def __iter__(self):
+        self.module.First()
+
+        while True:
+
+            yield getattr(self.module, self.function)
+
+            if not self.module.Next() > 0:
+                break
