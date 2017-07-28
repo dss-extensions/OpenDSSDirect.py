ef6c68a83071fef18e518756c28ec94ce3a240b5
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2017-07-28 11:50:06 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: BSD-Compatible License',
+        'License :: Other/Proprietary License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
