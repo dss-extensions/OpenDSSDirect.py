47fd64a90aecf67a5c0b43c6d10882921cc5bbee
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2017-08-05 15:30:05 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,12 @@ from codecs import open
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the README file
-with open(os.path.join(here, './docs/readme.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+try:
+    import pypandoc
+    long_description = pypandoc.convert('./README.md', 'rst')
+except (IOError, ImportError):
+    with open(os.path.join(here, './README.md'), encoding='utf-8') as f:
+        long_description = f.read()
 
 with open(os.path.join(here, 'opendssdirect', '_version.py'), encoding='utf-8') as f:
     version = f.read()
