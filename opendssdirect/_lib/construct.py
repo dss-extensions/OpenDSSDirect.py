8414fbf527e3c5b9d04cba5ddf9cefef706e7e77
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2017-08-25 09:38:56 -0600

    Update Monitors.ByteStream based on #2
    Use data extraction code provided by @Muxelmann

--- opendssdirect/_lib/construct.py
+++ opendssdirect/_lib/construct.py
@@ -6,7 +6,9 @@ import json
 from collections import OrderedDict
 from functools import partial
 import ctypes
+import struct
 import logging
+
 
 from .._compat import ModuleType, is_py2
 
@@ -15,6 +17,11 @@ from .core import VArg, VarArray, is_x64, is_delphi
 
 
 logger = logging.getLogger('opendssdirect.core')
+
+try:
+    import numpy as np
+except ImportError:
+    logger.warning("Unable to import numpy. Monitors.ByteStream may not work as expected")
 
 
 # Global modules and classes that can be populated by functions
@@ -279,49 +286,30 @@ def VarArrayFunction(f, mode, name, optional):
 
     elif varg.dtype == 0x2011 and var_arr.length != 0:
 
-        signature = ctypes.cast(var_arr.data, ctypes.POINTER(ctypes.c_int32)).contents.value
+        bs = [i for i in ctypes.cast(var_arr.data, ctypes.POINTER(ctypes.c_int8 * var_arr.length)).contents]
 
+        signature = int.from_bytes(bs[0:3], byteorder='little', signed=True)
         if signature != 43756:
-            logger.warning("ByteStream did not contain expected signature. Found {} but expected 43756".format(signature))
+            logger.warning(
+                'Monitor byte stream did not contain expected signature. Expected 43756 but found {signature} instead.'.format(
+                    signature=signature,
+                )
+            )
         else:
-            # data = ctypes.cast(var_arr.data, ctypes.POINTER(ctypes.c_int32 * 4))
-            # signature, version, size, mode = data.contents
-
-            p = ctypes.cast(var_arr.data, ctypes.POINTER(ctypes.c_int32))
-
-            a_ptr = ctypes.cast(p, ctypes.c_void_p)
-
-            a_ptr.value += ctypes.sizeof(p._type_)
-            version = ctypes.cast(a_ptr, ctypes.POINTER(ctypes.c_int32)).contents.value
-
-            a_ptr.value += ctypes.sizeof(p._type_)
-            size = ctypes.cast(a_ptr, ctypes.POINTER(ctypes.c_int32)).contents.value
-
-            a_ptr.value += ctypes.sizeof(p._type_)
-            mode = ctypes.cast(a_ptr, ctypes.POINTER(ctypes.c_int32)).contents.value
-
+            version = int.from_bytes(bs[4:7], byteorder='little', signed=True)
+            size = int.from_bytes(bs[8:11], byteorder='little', signed=True)
+            mode = int.from_bytes(bs[12:15], byteorder='little', signed=True)
             logger.debug("version={version}, size={size}, mode={mode}".format(version=version, size=size, mode=mode))
+            header = bytes(bs[16:271]).decode('ascii').rstrip('\0')
+            data_cols = header.count(',') + 1
 
-            a_ptr.value += ctypes.sizeof(p._type_)
-            header = ctypes.cast(a_ptr, ctypes.POINTER(ctypes.c_char * 256)).contents.value
-            header = [i.strip() for i in header.decode('ascii').split(',')]
-
-            a_ptr.value = a_ptr.value + 256 * ctypes.sizeof(p._type_)
-            data = ctypes.cast(a_ptr, ctypes.POINTER(ctypes.c_float * (size + 2)))
-
-            # TODO: Is there a better way to get the count of the number of Monitors?
-            import opendssdirect as dss
-            count = dss.Monitors.Count()
-
-            data = ctypes.cast(a_ptr, ctypes.POINTER(ctypes.c_float * (size + 2) * count))
-
-            l = dict()
-            for i in header:
-                l[i] = []
-
-            for row in data.contents[:]:
-                for i, v in enumerate(row[:]):
-                    l[header[i]].append(v)
+            data = []
+            for i in range(272, len(bs), 4):
+                byte_str = bytes(bs[i:i + 4])
+                val = struct.unpack('<f', byte_str)[0]
+                data.append(val)
+            data = np.array(data).reshape([-1, data_cols])
+            l = data
 
     elif var_arr.length == 0:
 
