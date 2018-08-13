af36fc4a799852541763d366d32735aa5a2e4214
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-08-13 15:17:53 -0600

    Update to v0.3.1

--- opendssdirect/__init__.py
+++ opendssdirect/__init__.py
@@ -3,8 +3,8 @@ This package tries to implement the OpenDSSDirect.py API using dss_CAPI instead
 of the official OpenDSS Direct DLL.
 '''
 from __future__ import absolute_import
+from ._version import __version__
 from . import dss, utils
 from .dss import *
-
 dss.dss = dss
 
