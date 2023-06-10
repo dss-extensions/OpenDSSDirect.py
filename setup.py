b959d642254d9a69731e0d1c55130b0072586a3b
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2023-06-10 01:35:03 -0300

    setup.py: Add lower bound (0.14.3) to DSS-Python's version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     author_email="me@kdheepak.com",
     license="BSD-compatible",
     packages=find_packages(),
-    install_requires=["dss_python==0.14.*"],
+    install_requires=["dss_python>=0.14.3,<0.15"],
     extras_require={
         "extras": ["pandas", "matplotlib", "networkx"],
         "dev": [
