b7ddbddd54a96746a13442107d62b34231228173
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2023-07-02 02:24:02 -0300

    Migrate DSSimComs to class (maybe we should just remove this one)

--- opendssdirect/DSSimComs.py
+++ opendssdirect/DSSimComs.py
@@ -1,13 +1,27 @@
-from ._utils import lib, CheckForError, get_float64_array
+from ._utils import api_util, Base
 
 
-def BusVoltage(Index):
-    return CheckForError(get_float64_array(lib.DSSimComs_BusVoltage, Index))
+class IDSSimComs(Base):
+    __slots__ = []
+    __name__ = "DSSimComs"
+    _api_prefix = "DSSimComs"
+    _columns = []
+
+    def BusVoltage(self, Index):
+        return self.CheckForError(
+            self._get_float64_array(self._lib.DSSimComs_BusVoltage, Index)
+        )
+
+    def BusVoltagepu(self, Index):
+        return self.CheckForError(
+            self._get_float64_array(self._lib.DSSimComs_BusVoltagepu, Index)
+        )
 
 
-def BusVoltagepu(Index):
-    return CheckForError(get_float64_array(lib.DSSimComs_BusVoltagepu, Index))
+_DSSimComs = IDSSimComs(api_util)
 
-
-_columns = []
+# For backwards compatibility, bind to the default instance
+BusVoltage = _DSSimComs.BusVoltage
+BusVoltagepu = _DSSimComs.BusVoltagepu
+_columns = _DSSimComs._columns
 __all__ = ["BusVoltage", "BusVoltagepu"]
