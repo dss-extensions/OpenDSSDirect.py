0dec1ebfd2e1882a0405726654feffd35a4b0a3a
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2023-07-03 04:29:32 -0300

    Reapply old numpy.complex fix

--- opendssdirect/YMatrix.py
+++ opendssdirect/YMatrix.py
@@ -28,7 +28,7 @@ class IYMatrix(Base):
             # return as (data, indices, indptr) that can fed into scipy.sparse.csc_matrix
             res = (
                 np.frombuffer(
-                    ffi.buffer(cValsPtr[0], nNz[0] * 16), dtype=np.complex
+                    ffi.buffer(cValsPtr[0], nNz[0] * 16), dtype=complex
                 ).copy(),
                 np.frombuffer(
                     ffi.buffer(RowIdxPtr[0], nNz[0] * 4), dtype=np.int32
